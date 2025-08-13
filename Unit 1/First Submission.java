0a913713ac62bf062669e4844857b032f58c40fa
King-Serious <70780344+King-Serious@users.noreply.github.com>
2025-08-13 08:33:47 -1000

    Create First Submission.java

---
+++ Unit 1/First Submission.java
@@ -0,0 +1,14 @@
+// This is where the magic happens
+
+public class Main {
+  public static void main(String[] args) {
+      //Task 1: Print "Hello World"
+      //This code will print out "Hello World"
+    System.out.print("Hello World");
+    //TASK 2: Print out "Hello World" and then print it again on the next line
+    //This code will also print out "Hello World" and moves the next "Hello World to the next line"
+    System.out.println("Hello World");
+    //This code will print out "Hello World"
+    System.out.print("Hello World");
+  }
+}
